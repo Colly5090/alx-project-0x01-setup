8cedcbbb4a457dc2a89eeb59da885b060254cdba
Collins Boakye <colly0087@gmail.com>
2025-07-17 11:37:57 -0700

    NextJs Advanced Structures mastery

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -6,27 +6,27 @@ export interface PostProps {
 }
 
 export interface UserProps {
-    id:       number;
-    name:     string;
-    username: string;
-    email:    string;
-    address:  Address;
-    phone:    string;
-    website:  string;
-    company:  Company;
-}
-
-export interface Address {
-    street:  string;
-    suite:   string;
-    city:    string;
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
     zipcode: string;
-    geo:     Geo;
-}
-
-export interface Geo {
-    lat: string;
-    lng: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
 
 export interface Company {
