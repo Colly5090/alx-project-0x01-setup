8c7f862b623fda8d0bf59138284f0cf6972479a2
Collins Boakye <colly0087@gmail.com>
2025-07-17 11:20:22 -0700

    NextJs Advanced Structures mastery

---
+++ alx-project-0x01/components/common/Button.tsx
@@ -0,0 +1,10 @@
+
+const Button: React.FC = () => {
+    return (
+        <div>
+            <h1>This will be button component</h1>
+        </div>
+    )
+}
+
+export default Button;
---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Footer: React.FC = () => {
+    return (
+        <header className="bg-blue-600 text-white shadow-md py-4">
+            <div className="container mx-auto flex justify-between items-center px-4">
+                <p>Copyright &copy; 2025</p>
+            </div>
+        </header>
+    );
+}
+
+export default Footer;
